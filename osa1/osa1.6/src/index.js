1eb0f7a9e2c379826a0a379f6646c22363ea5a67
Tatu Toikkanen <tatatofly@gmail.com>
2018-07-11 15:58:46 +0300

    Exercise 1.7

--- osa1/osa1.6/src/index.js
+++ osa1/osa1.6/src/index.js
@@ -16,18 +16,23 @@ const painikkeet = [
     }
 ]
 
+let palautteet = []
+let palautteetSum = 0
+
 const Title = ({text}) => (<div><h1>{text}</h1></div>)
 
 const Button = ({ handleClick, text}) => ( <button onClick={handleClick}>{text}</button>)
 
 const Statistic = ({ text, counter}) => ( <div><p>{text} {counter}</p></div>)
 
-const Statistics = ({hyva, neutraali, huono}) => {
+const Statistics = ({hyva, neutraali, huono, keskiarvo, prosentti}) => {
     return (
         <div>
             <Statistic text={painikkeet[0].text} counter={hyva} />
             <Statistic text={painikkeet[1].text} counter={neutraali} />
             <Statistic text={painikkeet[2].text} counter={huono} />
+            <Statistic text={"keskiarvo"} counter={keskiarvo} />
+            <Statistic text={"positiivisia"} counter={prosentti} />
         </div>
     )
 }
@@ -46,10 +51,15 @@ class App extends React.Component {
         return () => {
             if(vote === 1){
                 this.setState({hyv: arvo})
+                palautteet.push(1)
+                palautteetSum += 1
             } else if (vote === 2){
                 this.setState({neutrl: arvo})
+                palautteet.push(0)
             } else if (vote === 3){
                 this.setState({eihyv: arvo})
+                palautteet.push(-1)
+                palautteetSum -= 1
             } else {
                 console.log("Error: ", arvo)
             }
@@ -66,7 +76,7 @@ class App extends React.Component {
                     <Button handleClick={this.lisaaPalaute(this.state.eihyv + 1, 3)} text={painikkeet[2].text} />
                 </div>
                 <Title text={alaotsikko} />
-                <Statistics hyva={this.state.hyv} neutraali={this.state.neutrl} huono={this.state.eihyv} />
+                <Statistics hyva={this.state.hyv} neutraali={this.state.neutrl} huono={this.state.eihyv} keskiarvo={(palautteetSum / palautteet.length).toFixed(1)} prosentti={((this.state.hyv * 100)/palautteet.length).toFixed(1)+"%"}/>
             </div>
         )
     }
