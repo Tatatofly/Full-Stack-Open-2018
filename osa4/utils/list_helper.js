8ee182f5df3ffc631cc88a71db7e5a9c44714e33
Tatu Toikkanen <tatatofly@gmail.com>
2018-11-07 18:17:21 +0200

    Exercise 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,18 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.map((blog) => {
+    return blog.likes
+  })
+  const reducer = (sum, like) => {
+    return sum + like
+  }
+  return likes.length === 0 ? 0 : likes.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
