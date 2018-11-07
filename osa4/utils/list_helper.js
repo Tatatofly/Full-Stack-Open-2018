368f7461df7f921f2f13fcf0045b28a7010cadc5
Tatu Toikkanen <tatatofly@gmail.com>
2018-11-07 18:39:27 +0200

    Exercise 4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -12,7 +12,25 @@ const totalLikes = (blogs) => {
   return likes.length === 0 ? 0 : likes.reduce(reducer, 0)
 }
 
+const favoriteBlog  = (blogs) => {
+  const likes = blogs.map((blog) => {
+    return blog.likes
+  })
+  if(likes.length > 0){
+    const indexOfMostLikes = likes.reduce((best, x, i, like) => x > like[best] ? i : best, 0)
+    const theResult = {
+      title: blogs[indexOfMostLikes].title,
+      author: blogs[indexOfMostLikes].author,
+      likes: blogs[indexOfMostLikes].likes
+    }
+    return theResult
+  } else {
+    return {}
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
