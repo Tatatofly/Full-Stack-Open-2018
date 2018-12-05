40164e33415b81fd20e47ae0f8d98715de824b9b
Tatu Toikkanen <tatatofly@gmail.com>
2018-12-05 16:30:26 +0200

    Exercise 5.1

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      username: '',
+      password: '',
+      user: null
     }
   }
 
@@ -16,13 +20,74 @@ class App extends React.Component {
     )
   } 
 
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+  
+      this.setState({ username: '', password: '', user})
+    } catch(exception) {
+      this.setState({
+        error: 'Username or password is incorrect!',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    if (event.target.name === 'password') {
+      this.setState({ password: event.target.value })
+    } else if (event.target.name === 'username') {
+      this.setState({ username: event.target.value })
+    }
+  }
+
   render() {
+    const loginForm = () => (
+      <div>
+        <h2>Log in to application</h2>
+            <form onSubmit={this.login}>
+            <div>
+              Username: &nbsp;
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+            <div>
+              Password: &nbsp;
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+            <button type="submit">kirjaudu</button>
+          </form>
+      </div>
+  )
+
+  const blogList = () => (
+    <div>
+      <h2>blogs</h2>
+      {this.state.blogs.map(blog => 
+        <Blog key={blog._id} blog={blog}/>
+      )}
+    </div>
+  )
+
     return (
       <div>
-        <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
-        )}
+        {this.state.user === null && loginForm()}
+        {this.state.user !== null && blogList()}
       </div>
     );
   }
